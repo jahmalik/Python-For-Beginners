6b776013624f6ca3ca4e199605ff47179d906a69
Jamal Ahmad Malik <jahmalik1980@gmail.com>
2024-08-23 09:22:10 +0500

    Add files via upload

---
+++ basics/hello.py
@@ -0,0 +1,4 @@
+def main():
+    print("Striving For peace, Prosperity, Health and happiness")
+if __name__ == '__main__':
+    main()
---
+++ basics/variables.py
@@ -0,0 +1,11 @@
+def usman_variables():
+    # Variables Exercise
+    mystr = "E-Commerce Data Tracking Software" # 1st string variable 
+    mystr2 = "Application" # 2nd string variable 
+    myint = 100 # integet data type
+    myfloat = 10.75 # float data type
+    print(mystr + " " + mystr2, end= " | ") # string concatination 
+    print(myint, end= " | ") # int data type 
+    print(myfloat, end= " | ") # float data type
+if __name__ == '__main__': 
+    usman_variables()
