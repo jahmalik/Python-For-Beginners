db2694b786a1486db3a8b694c6fdfbbe18db86ba
Jamal Ahmad Malik <jahmalik1980@gmail.com>
2024-08-23 09:05:31 +0500

    Add files via upload

---
+++ number_integer_arithmetic_operators_addition.py
@@ -0,0 +1,15 @@
+def basic_number_addition():
+
+    # Int Addition
+    num1 = 654
+    num2 = 239
+    print(f'sum of {num1} and {num2} is: {num1+num2}')
+
+    # Float Addition
+    num3 = 75.25
+    num4 = 28.48
+    print(f'sum of {num3} and {num4} is: {num3+num4: .2f}')
+
+if __name__=='__main__':
+    basic_number_addition()
+
---
+++ number_integer_arithmetic_operators_subtraction.py
@@ -0,0 +1,14 @@
+def basic_number_subtraction():
+
+    # Int Subtraction
+    num1 = 654
+    num2 = 239
+    print(f'Difference of {num1} and {num2} is: {num1-num2}')
+
+    # Float Subtraction
+    num3 = 75.25
+    num4 = 28.48
+    print(f'Difference of {num3} and {num4} is: {num3-num4: .2f}')
+
+if __name__=='__main__':
+    basic_number_subtraction()
